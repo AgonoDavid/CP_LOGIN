e46d1e01e73e77389c7b479fbb55489b2ac10c00
David Agono <davidagono239@gmail.com>
2024-03-21 13:41:29 +0100

    ..

--- src/login/component/navbar.jsx
+++ src/login/component/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="lg:hidden w-full bg-white py-4 fixed top-0 z-50 border-b border-[#D7D7D7]">
       <nav className="flex justify-center items-center px-4">
-        <img src={logo} className="w-[20%]" alt="Logo" />
+        <img src={logo} className="w-[25%]" alt="Logo" />
       </nav>
     </div>
   );
